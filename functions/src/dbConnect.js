221f2e1838813ee03ae7b171f414460b7e3ed039
Midokhalil1 <midokhalil22@gmail.com>
2022-11-29 15:45:09 -0500

    firebase deploy

--- functions/src/dbConnect.js
+++ functions/src/dbConnect.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 import 'dotenv/config'
 
 export function dbConnect () {
-    const client = new MongoClient(process.env.MongoClient);
+    const client = new MongoClient(process.env.MONGO_URI);
     return client.db("mydatabase");
 }
 
